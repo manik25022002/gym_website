c771514541420b2fcdf9c19a0026d6313f10a8d9
manik25022002 <manikgarg25022002@gmail.com>
2024-01-09 20:14:05 +0530

    9jan 2024

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import Hero from './Components/hero/Hero';
 import Programs from './Components/programs/Programs';
 import './App.css';
 import Reasons from './Components/reasons/Reasons';
+import Plans from './Components/plans/Plans';
 
 function App() {
   return (
-    
+
     <div className="App">
-    <Hero/>
-      <Programs/>
-      <Reasons/>
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans />
     </div>
-   
-    
+
+
   );
 }
 
