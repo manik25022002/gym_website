97620a4db285b1b6df18dfb3d5366fb616e31bd9
manik25022002 <manikgarg25022002@gmail.com>
2024-01-09 16:48:08 +0530

    9jan 24

--- src/Components/programs/Programs.jsx
+++ src/Components/programs/Programs.jsx
@@ -1,7 +1,8 @@
 
 
 import './programs.css'
-// import { programsData } from '../../data/programsData'
+import { programsData } from '../../data/programsData'
+import RightArrow from '../../assets/rightArrow.png'
 const Programs = () => {
     return (
         <div id="programs" className="Programs">
@@ -15,6 +16,20 @@ const Programs = () => {
                 <span className='stroke-text' >to shape</span>
             </div>
 
+            <div className="program-categories">
+                {programsData.map((program) => (
+                    <div className="category">
+                        {program.image}
+                        <span>{program.heading}</span>
+                        <span>{program.details}</span>
+
+                        <div className="join-now">
+                            <span>Join Now</span>
+                            <img src={RightArrow} alt='' />
+                        </div>
+                    </div>
+                ))}
+            </div>
 
 
         </div>
