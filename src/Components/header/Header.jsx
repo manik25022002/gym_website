74c4b47b4a5b1aef40846687e9faa109049207cf
manik25022002 <manikgarg25022002@gmail.com>
2024-01-14 00:39:31 +0530

    fghj

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,22 +1,48 @@
 import './header.css'
 import Logo from '../../assets/logo.png'
 import Bars from '../../assets/bars.png'
-const Header = ()=>{
-    return(
+import { useState } from 'react';
+import {Link} from "react-scroll";
+const Header = () => {
 
-<div className="header">
-    <img  className='logo' src = {Logo} alt='manik' />
+    const mobile = window.innerWidth <= 768 ? true : false;
+    const [menuopen, setmenuopen] = useState(false);
 
-    <ul className='header-menu' >
-        <li>Home</li>
-        <li>Programs</li>
-        <li>Why Us</li>
-        <li>Plans</li>
-        <li>Testimonials</li>
-    </ul>
-    
-   
-</div>
+
+    return (
+        <>
+            <div className="header">
+                <img className='logo' src={Logo} alt='manik' />
+                {(menuopen === false && mobile === true) ? (
+                    <div
+                        style={{
+                            backgroundColor: 'var(--appColor)',
+                            padding: '0.5rem',
+                            borderRadius: '5px'
+                        }}
+                        onClick={() => setmenuopen(true)}
+                    >
+
+                        <img src={Bars} alt="" style={{ width: '1.5rem', height: '1.5rem' }} />
+
+                    </div>
+
+                )
+                    : (
+
+                        <ul className='header-menu' >
+                            <li onClick={() => setmenuopen(false)} >Home</li>
+                            <li onClick={() => setmenuopen(false)} >Programs</li>
+                            <li onClick={() => setmenuopen(false)} >Why Us</li>
+                            <li onClick={() => setmenuopen(false)} >Plans</li>
+                            <li onClick={() => setmenuopen(false)} >Testimonials</li>
+                        </ul>
+                    )}
+
+
+
+            </div >
+        </>
 
     )
 }
